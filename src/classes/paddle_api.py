bc213c1bf587718290d52f2333fa1640c515d73e
huyiteng <yitengh2@illinois.edu>
2023-03-21 17:28:26 -0500

    minor changes on paddle classes

--- src/classes/paddle_api.py
+++ src/classes/paddle_api.py
@@ -3,6 +3,17 @@ from classes.argument import *
 from classes.api import *
 from classes.database import TorchDatabase
 
+# TODO: create paddle_argument class
+class PaddleArgument(Argument):
+    # def __init__(self, arg_type: ArgType,arg_name: str = None) -> None:
+    #     super().__init__(arg_type, arg_value, arg_name)
+
+    def to_code(self, prefix="arg") -> str:
+        pass
+
+    def to_code_oracle(self, prefix="arg", oracle=OracleType.CRASH) -> str:
+        pass
+
 # TODO: create paddle_api class
 class PADDLEAPI(API):
     def __init__(self, api_name, record=None) -> None:
--- src/classes/paddle_library.py
+++ src/classes/paddle_library.py
@@ -1,5 +1,14 @@
 from classes.library import Library
 
+import numpy as np
+
+from classes.argument import Argument, ArgType
+from classes.paddle_api import PaddleAPI, PaddleArgument
+from classes.library import Library
+from classes.database import PaddleDatabase
+from constants.enum import OracleType
+from constants.keys import ERR_CPU_KEY, ERR_GPU_KEY, ERR_HIGH_KEY, ERR_LOW_KEY, ERROR_KEY, RES_CPU_KEY, RES_GPU_KEY, TIME_HIGH_KEY, TIME_LOW_KEY
+
 
 #TODO: create paddle_library class
 class TFLibrary(Library):
