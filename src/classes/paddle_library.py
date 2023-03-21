d6faa0214146ae6507abcb9a26932344a6e4fdf0
huyiteng <yitengh2@illinois.edu>
2023-03-21 17:41:24 -0500

    import PADDLEAPI

--- src/classes/paddle_library.py
+++ src/classes/paddle_library.py
@@ -3,7 +3,7 @@ from classes.library import Library
 import numpy as np
 
 from classes.argument import Argument, ArgType
-from classes.paddle_api import PaddleAPI, PaddleArgument
+from classes.paddle_api import PADDLEAPI, PaddleArgument
 from classes.library import Library
 from classes.database import PaddleDatabase
 from constants.enum import OracleType
@@ -12,4 +12,18 @@ from constants.keys import ERR_CPU_KEY, ERR_GPU_KEY, ERR_HIGH_KEY, ERR_LOW_KEY, 
 
 #TODO: create paddle_library class
 class TFLibrary(Library):
-    pass
+    @staticmethod
+    def generate_code(api: PADDLEAPI, oracle: OracleType) -> str:
+        code = ""
+        if oracle == OracleType.CRASH:
+            code += api.to_code_oracle(oracle=oracle)
+            return code
+        elif oracle == OracleType.CUDA:
+            code += api.to_code_oracle(oracle=oracle)
+            return code
+        elif oracle == OracleType.PRECISION:
+            code += api.to_code_oracle(oracle=oracle)
+            return code
+        else:
+            assert 0
+
