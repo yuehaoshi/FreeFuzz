1a4ad823c18d105fe64c9935ebb806dc49b6940f
Yuehao Shi <ashimoon97@gmail.com>
2023-03-30 12:04:58 -0500

    Fixed paddle write api name as torch issue

--- src/instrumentation/paddlepaddle/write_tools.py
+++ src/instrumentation/paddlepaddle/write_tools.py
@@ -3,11 +3,11 @@ import pymongo
 """
 You should configure the database
 """
-torch_db = pymongo.MongoClient(host="localhost", port=27017)["freefuzz-paddle"]
+paddle_db = pymongo.MongoClient(host="localhost", port=27017)["freefuzz-paddle"]
 
 def write_fn(func_name, params, input_signature, output_signature):
     params = dict(params)
-    out_fname = "torch." + func_name
+    out_fname = "paddle." + func_name
     params['input_signature'] = input_signature
     params['output_signature'] = output_signature
-    torch_db[out_fname].insert_one(params)
+    paddle_db[out_fname].insert_one(params)
